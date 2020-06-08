c6f0de29257060a51ba7cebf1850c54027de58e6
Amardeep Kumar <59433305+amardeepaariya@users.noreply.github.com>
2020-06-08 10:41:40 +0530

    Add files via upload

---
+++ webSocket-demoApp/src/test/java/com/example/webSocketdemoApp/WebSocketDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webSocketdemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
